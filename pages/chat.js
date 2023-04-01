f7acc314ef300022bd00caa92494d832b0a1a7a1
KiwiKid <KiwiKid@users.noreply.github.com>
2023-04-01 19:13:23 +1300

    more externals?

--- pages/chat.js
+++ pages/chat.js
@@ -2,8 +2,6 @@ import React from 'react';
 import Layout from '@/components/layout';
 import Chat from '@/components/chat'
 
-import dynamic from 'next/dynamic'
-
 /*const DynamicChat = dynamic(() => import(), {
   loading: () => <p>Loading...</p>,
 })*/
